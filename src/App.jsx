8257fd8097a38dd500e63bb764ebe31eaea4cd8c
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-24 22:08:03 +0330

    feat(toast-message): show toast-message when a new contact is added

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ const App = () => {
         <AddContactPage
           setCurrentPage={setCurrentPage}
           setContacts={setContacts}
+          showToast={showToast}
         />
       )}
     </>
