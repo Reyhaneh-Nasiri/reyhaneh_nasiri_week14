11562873624171cd5a296aba1652d21e2734b691
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-26 18:37:11 +0330

    feat(view-contact): add structure and style

--- src/pages/ViewContactPage/ViewContactPage.jsx
+++ src/pages/ViewContactPage/ViewContactPage.jsx
@@ -1,6 +1,21 @@
+import styles from "./ViewContactPage.module.css"
 const ViewContactPage = () => {
   return (
-    <div>ViewContactPage</div>
+    <div className={styles.container}>
+        <div className={styles.header}>
+            <i className="fa-solid fa-arrow-left"></i>
+            <i class="fa-solid fa-pen-to-square"></i>
+        </div>
+        <ul className={styles.information}>
+            <li>Name: <span>Reyhaneh</span></li>
+            <li>Email: <span>r@gmail.com</span></li>
+            <li>Phone: <span>9999</span></li>
+            <li>Job: <span>Programmer</span></li>
+        </ul>
+        <div className={styles.footer}>
+            <button>Delete</button>
+        </div>
+    </div>
   )
 }
 
